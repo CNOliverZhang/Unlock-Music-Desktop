b55692fb5b1d7d2ed73155b293dbfb0d340f1098
CNOliverZhang <cnoliverzhang@gmail.com>
2019-12-22 23:19:25 +0800

    Simplified dependencies.

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -6,11 +6,9 @@ import fontawesome from '@fortawesome/fontawesome'
 import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
 import solid from '@fortawesome/fontawesome-free-solid'
 import regular from '@fortawesome/fontawesome-free-regular'
-import brands from '@fortawesome/fontawesome-free-brands'
 
 fontawesome.library.add(solid)
 fontawesome.library.add(regular)
-fontawesome.library.add(brands)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
